69abea57462c88e9e9a33ea19711248fe728903b
OlgaSergejceva <justyna@inbox.lv>
2020-03-09 19:29:40 +0200

    some changes in the changeCalculator.java

--- src/Lab/saturday/ChangeCalculator.java
+++ src/Lab/saturday/ChangeCalculator.java
@@ -27,6 +27,7 @@ public class ChangeCalculator {
 		int price, received;
 		int change;
 		int pay50, pay20, pay10, pay5, pay2, pay1;
+		System.out.println("Test");
 		
 		//input
 		//declare a variable of type Scanner
